52e7c4caa9368393ca8dce462e9727ea938ab9ce
Dwight Guth <dwight.guth@runtimeverification.com>
2018-05-07 12:27:41 -0500

    update wallet to not use low level calls

--- test/contracts/Wallet.cpp
+++ test/contracts/Wallet.cpp
@@ -337,17 +337,17 @@ contract multisig {
 	// Funds has arrived into the wallet (record how much).
 	event Deposit(address from, uint value);
 	// Single transaction going out of the wallet (record who signed for it, how much, and to whom it's going).
-	event SingleTransact(address owner, uint value, address to, bytes data);
+	event SingleTransact(address owner, uint value, function () external payable func);
 	// Multi-sig transaction going out of the wallet (record who signed for it last, the operation hash, how much, and to whom it's going).
-	event MultiTransact(address owner, bytes32 operation, uint value, address to, bytes data);
+	event MultiTransact(address owner, bytes32 operation, uint value, function () external payable func);
 	// Confirmation still needed for a transaction.
-	event ConfirmationNeeded(bytes32 operation, address initiator, uint value, address to, bytes data);
+	event ConfirmationNeeded(bytes32 operation, address initiator, uint value, function () external payable func);
 
 	// FUNCTIONS
 
 	// TODO: document
 	function changeOwner(address _from, address _to) external;
-	function execute(address _to, uint _value, bytes _data) external returns (bytes32);
+	function execute(function () external payable _func, uint _value) external returns (bytes32);
 	function confirm(bytes32 _h) returns (bool);
 }
 
@@ -360,9 +360,8 @@ contract Wallet is multisig, multiowned, daylimit {
 
 	// Transaction structure to remember details of transaction lest it need be saved for a later call.
 	struct Transaction {
-		address to;
+		function () external payable func;
 		uint value;
-		bytes data;
 	}
 
 	// METHODS
@@ -389,30 +388,29 @@ contract Wallet is multisig, multiowned, daylimit {
 	// If not, goes into multisig process. We provide a hash on return to allow the sender to provide
 	// shortcuts for the other confirmations (allowing them to avoid replicating the _to, _value
 	// and _data arguments). They still get the option of using them if they want, anyways.
-	function execute(address _to, uint _value, bytes _data) external onlyowner returns (bytes32 _r) {
+	function execute(function () external payable _func, uint _value) external onlyowner returns (bytes32 _r) {
 		// first, take the opportunity to check that we're under the daily limit.
 		if (underLimit(_value)) {
-			SingleTransact(msg.sender, _value, _to, _data);
+			SingleTransact(msg.sender, _value, _func);
 			// yes - just execute the call.
-			_to.call.value(_value)(_data);
+			_func.value(_value)();
 			return 0;
 		}
 		// determine our operation hash.
-		_r = keccak256(8, _to, _value, _data, block.number);
-		if (!confirm(_r) && m_txs[_r].to == 0) {
-			m_txs[_r].to = _to;
+		_r = keccak256(8, _func, _value, block.number);
+		if (!confirm(_r) && address(m_txs[_r].func) == 0) {
+			m_txs[_r].func = _func;
 			m_txs[_r].value = _value;
-			m_txs[_r].data = _data;
-			ConfirmationNeeded(_r, msg.sender, _value, _to, _data);
+			ConfirmationNeeded(_r, msg.sender, _value, _func);
 		}
 	}
 
 	// confirm a transaction through just the hash. we use the previous transactions map, m_txs, in order
 	// to determine the body of the transaction from the hash provided.
 	function confirm(bytes32 _h) onlymanyowners(_h) returns (bool) {
-		if (m_txs[_h].to != 0) {
-			m_txs[_h].to.call.value(m_txs[_h].value)(m_txs[_h].data);
-			MultiTransact(msg.sender, _h, m_txs[_h].value, m_txs[_h].to, m_txs[_h].data);
+		if (address(m_txs[_h].func) != 0) {
+			m_txs[_h].func.value(m_txs[_h].value)();
+			MultiTransact(msg.sender, _h, m_txs[_h].value, m_txs[_h].func);
 			delete m_txs[_h];
 			return true;
 		}
