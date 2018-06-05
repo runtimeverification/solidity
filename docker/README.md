1ebd4a6571fd4f9ae2952a4d52ecd77b1845918f
Yiyi Wang <shd101wyy@sina.com>
2018-06-05 17:52:33 -0500

    Add docker support

---
+++ docker/README.md
@@ -0,0 +1,78 @@
+# Solidity to IELE compiler docker 
+
+
+<!-- @import "[TOC]" {cmd="toc" depthFrom=2 depthTo=6 orderedList=false} -->
+
+<!-- code_chunk_output -->
+
+* [Build docker image](#build-docker-image)
+* [Start container as an HTTP server](#start-container-as-an-http-server)
+* [Run container that mounts local directory](#run-container-that-mounts-local-directory)
+
+<!-- /code_chunk_output -->
+
+
+## Build docker image 
+
+```bash
+# pull from docker hub
+$ docker pull runtimeverification/sol2iele:latest
+
+# or build from Dockerfile
+$ docker build -t runtimeverification/sol2iele:latest .
+```
+
+## Start container as an HTTP server
+
+Run the following command to start the server:
+
+```bash
+$ docker run -ti --rm -p 7777:8080 runtimeverification/sol2iele:latest
+```
+
+Then you can compile solidity code to iele by sending a `POST` request to the server:
+
+```bash
+# Assume the container is running from the previous step
+curl -X POST --data '{"method": "sol2iele_asm", "params": ["main.sol", {"main.sol":"code1...","./dir/dependency.sol":"code2..."}]}' localhost:7777
+```
+
+
+For example, assume we have two solidity files: 
+
+```solidity
+// owned.sol
+pragma solidity ^0.4.9;
+contract owned {
+    function owned() { owner = msg.sender; }
+    address owner;
+}
+
+// mortal.sol
+pragma solidity ^0.4.9;
+import "./owned.sol";
+contract mortal is owned{
+    function kill() {
+        selfdestruct(owner);
+    }
+}
+```
+
+then we can compile this two files like below:
+
+```bash
+$ curl -X POST --data '{"method": "sol2iele_asm", "params": ["mortal.sol", {"owned.sol":"pragma solidity ^0.4.9;\ncontract owned {\n    function owned() { owner = msg.sender; }\n    address owner;\n}","mortal.sol":"pragma solidity ^0.4.9;\nimport \"./owned.sol\";\ncontract mortal is owned{\n    function kill() {\n        selfdestruct(owner);\n    }\n}"}]}' localhost:7777
+```
+
+## Run container that mounts local directory  
+
+```bash
+$ docker run -it --rm -v absolute_path_to_local_dir:/src runtimeverification/sol2iele bash
+```
+
+You will then enter the interactive mode in docker container.
+
+```bash
+$ cd /src     # this is where you mount your local directory
+$ isolc --asm file_you_want_to_compile.sol  # compile a solidity file
+```
---
+++ docker/server.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import subprocess
+import json
+import tempfile
+import shutil
+import os
+
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+
+    # GET
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-Type','application/json')
+        self.end_headers()
+
+        # Write content as utf-8 data
+        self.wfile.write(bytes('{"success":true, "data": "Server is running!"}', "utf8"))
+        return
+    
+    def do_POST(self):
+        dirpath = None
+        try:
+            content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+            post_data = self.rfile.read(content_length).decode('utf8') # <--- Gets the data itself
+            post_json = json.loads(post_data)
+            method = post_json["method"]
+            params = post_json["params"]
+            if method == "sol2iele_asm":
+                dirpath = tempfile.mkdtemp()
+                main_file_path = params[0]
+                filepath_code_map = params[1]
+                for filepath in filepath_code_map:
+                    code = filepath_code_map[filepath]
+                    with open(os.path.join(dirpath, filepath), "w") as outfile:
+                        outfile.write(code)
+                p = subprocess.Popen(["isolc", "--asm", os.path.join(dirpath, main_file_path)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                (result, error) = p.communicate()
+                if error != None:
+                    raise Exception(error)
+                else:
+                    response_json = json.dumps({"success":True, "data": result.decode('utf8')})
+            else:
+                raise Exception("Invalid method %s" % (method))
+
+            if dirpath != None:
+                shutil.rmtree(dirpath)
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(response_json, 'utf8'))
+        except Exception as error:
+            if dirpath != None:
+                shutil.rmtree(dirpath)
+            self.send_response(500)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(bytes('{"success": false, "data": "%s"}' % (repr(error)), 'utf8'))
+def run():
+    print('starting server...')
+    HOST = '0.0.0.0' # '127.0.0.1'
+    PORT = 8080
+    # Server settings
+    server_address = (HOST, PORT)
+    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+    print('running server...')
+    httpd.serve_forever()
+
+run()
