e8745573d3e303dbb600e677b14e187637090201
Theodoros Kasampalis <kasampalis.theodoros@gmail.com>
2021-03-29 21:13:28 -0500

    Cleaning up tests (and other files) that were deleted upstream for version 0.8.0

--- scripts/docker_build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-docker build -t ethereum/isolc:build -f scripts/Dockerfile .
-tmp_container=$(docker create ethereum/isolc:build sh)
-mkdir -p upload
-docker cp ${tmp_container}:/usr/bin/isolc upload/isolc-static-linux
