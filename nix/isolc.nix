700e168638dfa59300c753edffec1a8971501ae7
Thomas Tuegel <ttuegel@mailbox.org>
2021-04-29 15:22:58 -0500

    Remove CVC4

--- nix/isolc.nix
+++ nix/isolc.nix
@@ -7,20 +7,14 @@
 , python3
 , z3Support ? true
 , z3 ? null
-, cvc4Support ? true
-, cvc4 ? null
-, cln ? null
-, gmp ? null
 , llvm
 , kiele
 , makeWrapper
 }:
 
-# compiling source/libsmtutil/CVC4Interface.cpp breaks on clang on Darwin,
 # general commandline tests fail at abiencoderv2_no_warning/ on clang on NixOS
 
 assert z3Support -> z3 != null && lib.versionAtLeast z3.version "4.6.0";
-assert cvc4Support -> cvc4 != null && cln != null && gmp != null;
 
 let
   jsoncppVersion = "1.9.3";
@@ -63,16 +57,14 @@ let
 
     cmakeFlags = [
       "-DBoost_USE_STATIC_LIBS=OFF"
+      "-DUSE_CVC4=OFF"
     ] ++ lib.optionals (!z3Support) [
       "-DUSE_Z3=OFF"
-    ] ++ lib.optionals (!cvc4Support) [
-      "-DUSE_CVC4=OFF"
     ];
 
     nativeBuildInputs = [ cmake makeWrapper ];
     buildInputs = [ boost llvm ]
-      ++ lib.optionals z3Support [ z3 ]
-      ++ lib.optionals cvc4Support [ cvc4 cln gmp ];
+      ++ lib.optionals z3Support [ z3 ];
 
     postFixup = ''
       wrapProgram $out/bin/isolc --prefix PATH : '${host-PATH}'
