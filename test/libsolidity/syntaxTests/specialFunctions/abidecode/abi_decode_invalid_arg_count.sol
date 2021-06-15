5029846ea43ada1d57c86c8291e8c8ee3642327a
Maria Kotsifakou <kotsifakou.maria@gmail.com>
2021-06-15 21:36:50 +0000

    Updated failing execution tests: syntaxTests/{specialFunctions, structs}.

--- test/libsolidity/syntaxTests/specialFunctions/abidecode/abi_decode_invalid_arg_count.sol
+++ test/libsolidity/syntaxTests/specialFunctions/abidecode/abi_decode_invalid_arg_count.sol
@@ -7,6 +7,8 @@ contract C {
 }
 // ----
 // TypeError 5782: (46-58): This function takes two arguments, but 0 were provided.
+// TypeError 2699: (75-83): msg.data is not supported in IELE. For more information, including potential workarounds, see README-IELE-SUPPORT.md
 // TypeError 5782: (64-84): This function takes two arguments, but 1 were provided.
+// TypeError 2699: (101-109): msg.data is not supported in IELE. For more information, including potential workarounds, see README-IELE-SUPPORT.md
 // TypeError 5782: (90-122): This function takes two arguments, but 3 were provided.
 // TypeError 6444: (111-115): The second argument to "abi.decode" has to be a tuple of types.
