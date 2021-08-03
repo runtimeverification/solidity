00d96d14076d72d2a92f01edac10b3837539c437
Maria Kotsifakou <kotsifakou.maria@gmail.com>
2021-08-03 21:29:09 +0000

    Updated failing syntax tests in directory array/length. Removed now unnecessary (identical after merge) test.

--- test/libsolidity/syntaxTests/array/length/bytes32_too_large.sol
+++ test/libsolidity/syntaxTests/array/length/bytes32_too_large.sol
@@ -2,4 +2,5 @@ contract C {
     bytes32[8**90] ids;
 }
 // ----
-// TypeError 5462: (25-30): Invalid array length, expected integer literal or constant expression.
+// TypeError 7676: (0-38): Contract requires too much storage.
+// TypeError 1534: (17-35): Type too large for storage.
