6ee0e935b874d174f7508d6140988cb1e26d5b9b
Dwight Guth <dwight.guth@runtimeverification.com>
2021-11-02 14:53:17 -0500

    update dockerfile

--- env/soltest.sh
+++ env/soltest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd solidity
-./build/test/soltest --report_sink=build/report.xml --report_level=detailed --report_format=XML --log_level=all `cat test/failing-exec-tests` `cat test/out-of-scope-exec-tests` `cat test/unimplemented-features-tests` -- --enforce-no-yul-ewasm --ipcpath ~/.midnight-wallet/midnight.ipc --testpath test --wallet-id "ce3bbd3e" --private-from-address "m-test-shl-ad1x4k38xhcz4nvjv75cz5nvs447zed9f8mpa8sat2q8j95q9twjd7ku7te3uyuz94r6km0zwrwcnl"
+./build/test/soltest --report_sink=build/report.xml --report_level=detailed --report_format=XML --log_level=all `cat test/failing-exec-tests` `cat test/out-of-scope-exec-tests` `cat test/unimplemented-features-tests` -- --enforce-no-yul-ewasm --ipcpath ~/.midnight-wallet/midnight.ipc --testpath test --wallet-id "ce3bbd3e" --private-from-address "m-test-shl-ad15lx6zqyg9dw7zjwlhq32wvza6f548upx6x74d8lfclr7s9u9vh853ua3eyhsd5xgeuywu9l6g39"
